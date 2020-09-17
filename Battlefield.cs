133fbbf6b2ae7ebe4426a800d31ce942093d11d1
Brooke McBain <mcbainbrooke@gmail.com>
2020-09-17 08:11:28 -0500

    message

--- Battlefield.cs
+++ Battlefield.cs
@@ -8,36 +8,26 @@ namespace RobotsVsDinosaurs
 {
     class Battlefield
     {
-        Herd newDino = new Herd();
-        Fleet newRobot = new Fleet();
-        Dinosaur dinoAttack = new Dinosaur();
-        Robot robotAttack = new Robot();
-        Weapon weapon = new Weapon(); 
+        public Herd newDino = new Herd();
+        public Fleet newRobot = new Fleet();
+        public Dinosaur dinoAttack = new Dinosaur(); 
+        public Robot robotAttack = new Robot();
+        
+
+
 
         public Battlefield()
-
-        { 
-
-
-            //fleet.listName[0].AttackDino();
-
-        }
-        public void Introduction()
         {
             Console.WriteLine("ROBOTS VS. DINOSAURS");
             Console.WriteLine("Player 1 will be:" + newDino.dinosaurs);
-            Console.WriteLine("Player 2 will be:" + newRobot.robots); 
+            Console.WriteLine("Player 2 will be:" + newRobot.robots);
+            
+            dinoAttack.Dinosaur(robotAttack); 
+            robotAttack.AttackDino(dinoAttack);
+            dinoAttack.attackPower(robotAttack); 
 
         }
-        public void BattleMode()
-        {
-            }
-        }
-
-        public void EndGame()
-        {
-            // win game message 
-        }
+            
     }
 
 }
